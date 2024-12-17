46f21f6a02d0564568876d2525ea579de2fb94df
Bread-ou <93944942+Bread-ou@users.noreply.github.com>
2024-12-17 08:08:49 +0300

    Finished work

--- BasicsWork/Program.cs
+++ BasicsWork/Program.cs
@@ -25,7 +25,7 @@ single character (a-z,1-9,symbols, etc..) instead of an array of characters like
 Chars are used instead of a string (of a single character) to save on memory as well. */
 
 
-// C Variables task 1
+// C# Variables Task 1
 
 // camelCase
 string studentName1 = "Bread";
@@ -36,4 +36,44 @@ string student1FavoriteSubject = "Chemistry";
 string student_name2 = "Donut";
 double gpa_student_2 = 1.87;
 string studnet2_favorite_subject = "Home";
+
+// C# Variables Task 2
+// Area of rectangle = h x w
+
+/* When declaring variables using const instead of a regular declaration 
+the declared variable cannot be modified. These variables can still be used to do math,
+however you must keep in mind that they will not change. A const variable must also always
+be assigned a value that is not null.*/
+
+const double width = 5.5;
+double height = 3.2;
+
+double answer = 0;
+const double constAnswer = 0;
+
+answer = width * height; 
+
+// The expression below is incorrect because constAnswer is a const variable
+//constAnswer = width * height;
+
+Console.WriteLine("The Area of the Rectangle with heigth (3.2) and width (5.5) is: "+answer);
+//Console.WriteLine("The Area of the Rectangle with heigth (3.2) and width (5.5) is: "+constAnswer);
+
+
+// C# Control Flow
+
+// Get time of day in hours and store it
+DateTime currentTime = new DateTime(DateTime.Now.TimeOfDay.Ticks);
+int timeInHours = Convert.ToInt32(currentTime.ToString("HH"));
+     
+
+if(timeInHours >= 11 && timeInHours <= 14)
+{
+    Console.WriteLine("Time for Lunch!");
+}
+
+Console.WriteLine(timeInHours);
+
+
+
 
